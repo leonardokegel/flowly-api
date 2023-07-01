ddf332886b9410bea92df859db8a3e7b325528fe
Pedro Freitas <pedrinfreitas@gmail.com>
2023-07-01 10:44:08 -0300

    feat: conexão com postgresql aws

--- knexfile.js
+++ knexfile.js
@@ -3,23 +3,13 @@ require("dotenv").config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-//   config = {
-//     client: "pg",
-//     connection: {
-//       connectionString: process.env.DATABASE_URL,
-//     },
-//   };
+
 module.exports = {
   development: {
     client: "pg",
     connection: {
-      connectionString: process.env.DEV_ENV_DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
-      // host: process.env.DEV_ENV_HOST,
-      // port: process.env.DEV_ENV_PORT,
-      // user: process.env.DEV_ENV_USERNAME,
-      // password: process.env.DEV_ENV_PASSWORD,
-      // database: process.env.DEV_ENV_DATABASE,
     },
     migrations: {
       directory: __dirname + "/src/db/migrations",
@@ -32,13 +22,8 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      connectionString: process.env.ENV_DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
-      // host: process.env.ENV_HOST,
-      // port: process.env.ENV_PORT,
-      // user: process.env.ENV_USERNAME,
-      // password: process.env.ENV_PASSWORD,
-      // database: process.env.ENV_DATABASE,
     },
     migrations: {
       directory: __dirname + "/src/db/migrations",
