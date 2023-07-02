3ff11241fbe2211420559df572ccc029ee5a9a51
Leonardo Kegel <leonardo.kegel@gmail.com>
2023-07-02 10:58:59 -0300

    feat: get contratos

--- src/routes/contratosRouter.js
+++ src/routes/contratosRouter.js
@@ -4,37 +4,69 @@ const knex = require("../db/knex");
 const contratosRouter = express.Router();
 
 contratosRouter.get("/contratos", express.json(), (req, res) => {
-    knex
-      .select("*")
-      .from("contratos")
-      .then((contratos) => res.status(200).json(contratos))
-      .catch(() => {
-        res.status(500).json({
-          mensagem: "Internal Server Error!",
-        });
+  knex
+    .select("*")
+    .from("contratos")
+    .then((contratos) => res.status(200).json(contratos))
+    .catch(() => {
+      res.status(500).json({
+        mensagem: "Internal Server Error!",
       });
-  });
+    });
+});
 
 contratosRouter.get("/contratos/:id_cliente", express.json(), (req, res) => {
   let id_cliente = +req.params.id_cliente;
   knex("contratos")
     .where({ clienteId: id_cliente })
     .then((contratos) => {
-        res.status(200).json(contratos);
+      if (contratos.length > 0) {
+        knex("clientes")
+          .where({ id: contratos[0].clienteId })
+          .then((cliente) => {
+            const response = contratos.map((e) => (
+              {...e, cliente: cliente[0]}
+            ));
+            res.status(200).json(response);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              mensagem: "Internal Server Error!",
+            });
+          });
+      } else {
+        res.status(200).json([]);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        mensagem: "Internal Server Error!",
+      });
     });
 });
 
 contratosRouter.post("/contratos/:id_cliente", express.json(), (req, res) => {
   let id_cliente = +req.params.id_cliente;
   knex("contratos")
-    .insert({
+    .insert(
+      {
         clienteId: id_cliente,
         status: req.body.status,
-        titulo: req.body.titulo
-    }, ['clienteId', 'titulo', 'status'])
+        titulo: req.body.titulo,
+      },
+      ["clienteId", "titulo", "status"]
+    )
     .then((contratos) => {
-        res.status(200).json(contratos[0])
-    });
+      res.status(200).json(contratos[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        mensagem: "Internal Server Error!",
+      });
+    });;
 });
 
 module.exports = contratosRouter;
